59a934e1b046eadf06297c3b98a082bea24a2dd4
hayeke <ohjackwang@gmail.com>
2017-06-22 20:34:36 +0800

    在item.py中增加了数据结构定义

--- quotetutorial/items.py
+++ quotetutorial/items.py
@@ -3,10 +3,9 @@
 # Define here the models for your scraped items
 #
 # See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
+# http://doc.scrapy.org/en/latest/topics/items.html\
 
 import scrapy
-
 class QuoteItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
