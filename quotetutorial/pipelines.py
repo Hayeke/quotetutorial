629de0343f81fe8146dbc5459c5ecb5fda88b373
hayeke <ohjackwang@gmail.com>
2017-06-23 14:46:27 +0800

    在pipelines.py中增加了数据处理

--- quotetutorial/pipelines.py
+++ quotetutorial/pipelines.py
@@ -4,7 +4,8 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from  scrapy import  crawler
+import  pymongo
 
 class TextPipeline(object):
     def __init__(self):
@@ -12,27 +13,31 @@ class TextPipeline(object):
     def process_item(self, item, spider):
         if item["text"]:
             if len(item["text"])>self.limit:
-                item["text"] = item["text"].[0:self.limit].rstrip()+"..."
+                item["text"] = item["text"][0:self.limit].rstrip()+"..."
             return item
         else:
             return DropItem("")
 
+
+
 class MongoPipeline(object):
-    def __init__(self,mongo_url,mongo_db):
-        self.mongo_url = mongo_url
+    def __init__(self,mongo_uri,mongo_db):
+        self.mongo_uri = mongo_uri
         self.mongo_db = mongo_db
 
     @classmethod
-    def from_crawler(self,crawler):##自带方法，可以从settings里获取配置信息;类方法，返回一个class对象
+    def from_crawler(cls, crawler):  ##自带方法，可以从settings里获取配置信息;类方法，返回一个class对象
         return cls(
-            mongo_url=crawler.settings.get("MONGO_URL"),##在settings加入这两个变量
+            mongo_uri=crawler.settings.get("MONGO_URI"),  ##在settings加入这两个变量
             mongo_db=crawler.settings.get("MONGO_DB")
         )
     #open_spider,定义spider启动时的操作,初始化声明
     def open_spider(self,spider):
-        self.client = pymongo.MongoClient(self.mongo_url)
+        self.client = pymongo.MongoClient(self.mongo_uri)
         self.db = self.client[self.mongo_db]
 
     def process_item(self,item,spider):
         self.db["quotes"].insert(dict(item))
         return item
+    def close_spider(self,spider):
+        self.client.close()
--- quotetutorial/settings.py
+++ quotetutorial/settings.py
@@ -14,7 +14,7 @@ BOT_NAME = 'quotetutorial'
 SPIDER_MODULES = ['quotetutorial.spiders']
 NEWSPIDER_MODULE = 'quotetutorial.spiders'
 
-MONGO_URL="localhost"
+MONGO_URI="localhost"
 MONGO_DB="quotestutorial"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
@@ -66,9 +66,10 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'quotetutorial.pipelines.QuotetutorialPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'quotetutorial.pipelines.TextPipeline': 300,
+   'quotetutorial.pipelines.MongoPipeline': 400
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
